42ee2fd371b08808b6fac52d2b6cf69781fae805
LeomaiaJr <leomaiajr@hotmail.com>
2021-04-26 11:31:56 -0300

    adicionar movimento do player

--- programacao/my_game/game.js
+++ programacao/my_game/game.js
@@ -19,36 +19,71 @@ var game = new Phaser.Game(config);
 
 function preload() {
     this.load.image('classroom', '../../cenario/classroom.png');
-    this.load.spritesheet('player', 'sprite.png', { frameWidth: 376.8, frameHeight: 690 });
+    this.load.spritesheet('playerDown', '../../sprites/down.png', { frameWidth: 249, frameHeight: 690 });
+    this.load.spritesheet('playerRunning', '../../sprites/running.png', { frameWidth: 515, frameHeight: 690 });
+    this.load.spritesheet('playerRunningL', '../../sprites/runningL.png', { frameWidth: 515, frameHeight: 690 });
+    this.load.spritesheet('playerStand', '../../sprites/stand.png', { frameWidth: 262, frameHeight: 690 });
+    this.load.spritesheet('playerJump', '../../sprites/jump.png', { frameWidth: 343, frameHeight: 545 });
 }
 
 function create() {
     this.add.image(400, 300, 'classroom').setScale(1.5)
 
-    player = this.physics.add.sprite(100, 450, 'player', 0);
+    player = this.physics.add.sprite(801, 450, 'playerStand').setScale(0.2)
+    player.setGravityY(500)
 
-    player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
     this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'player', frame: 4 }],
-        frameRate: 20
-    });
-
-    this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
+        frames: this.anims.generateFrameNumbers("playerRunning"),
+        frameRate: 10
     });
+    this.anims.create({
+        key: 'down',
+        frames: this.anims.generateFrameNumbers("playerDown"),
+        frameRate: 10
+    });
+    this.anims.create({
+        key: 'stand',
+        frames: this.anims.generateFrameNumbers("playerStand"),
+        frameRate: 10
+    });
+    this.anims.create({
+        key: 'jump',
+        frames: this.anims.generateFrameNumbers("playerJump"),
+        frameRate: 10
+    });
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers("playerRunningL"),
+        frameRate: 10
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update() { }
+function update() {
+
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    }
+    else if (cursors.down.isDown) {
+        player.setVelocityX(0);
+        player.anims.play('down', true);
+    }
+    else if (cursors.up.isDown && player.body.y == 462) {
+        player.setVelocityY(-400);
+        player.setVelocityX(0);
+        player.anims.play('jump', true);
+    }
+    else {
+        player.setVelocityX(0);
+        player.anims.play('stand', true);
+    }
+}
