4888df4afdbea29c0798920ad804a97a693a87d8
LeomaiaJr <leomaiajr@hotmail.com>
2021-04-30 09:39:52 -0300

    atualizar classe da cateririnha

--- programacao/utils/bullet.js
+++ programacao/utils/bullet.js
@@ -1,27 +1,14 @@
-let Bullet = new Phaser.Class({
-    Extends: Phaser.GameObjects.Image,
-    initialize:
-        function Bullet(scene) {
-            Phaser.GameObjects.Image.call(this, scene, 0, 0, 'id_card'); 
-            
-            this.speed = Phaser.Math.GetSpeed(600, -1);
-        },
-
-    fire: function (x, y, pos) {
-        this.speed = pos == 'R' ? Phaser.Math.GetSpeed(600, 1) : Phaser.Math.GetSpeed(600, -1);
-
-        this.setPosition(pos == 'R' ? x + 100 : x, y + 50);
-        this.setActive(true);
-        this.setVisible(true);
-    },
-
-    update: function (time, delta) {
-        this.x += this.speed * delta;
-
-        if (this.x > 820) {
-            this.setActive(false);
-            this.setVisible(false);
-        }
+class Bullet {
+    constructor(scene, x, y, texture, scale) {
+        this.x = x
+        this.y = y
+        this.bullet = scene.physics.add.sprite(x, y, texture).setScale(scale)
+        this.xSpeed = 600;
+        this.bullet.setGravityY(-200)
+        this.bullet.setCollideWorldBounds(false);
     }
 
-});
+    fire(pos) {
+        this.bullet.setVelocityX(pos == 'R' ? this.xSpeed : -this.xSpeed);
+    }
+}
