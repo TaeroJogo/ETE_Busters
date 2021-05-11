8d1cf39624937495afce6b800224fefbb0af8b7d
LeomaiaJr <leomaiajr@hotmail.com>
2021-05-11 16:42:16 -0300

    adicionar sons de hover nos botões de select

--- programacao/my_game/scenes/menu.scene.js
+++ programacao/my_game/scenes/menu.scene.js
@@ -10,9 +10,22 @@ class MenuScreen extends Phaser.Scene {
     preload() {
         this.load.image('background', '../res/cenario/menuPhoto.png')
         this.title = new GameText(this, 80, 20, 'ETE BUSTERS', '120px', '#FFFFFF', 'Georgia, "Goudy Bookletter 1911", Times, serif')
+
+        this.load.audio('hoverSound', '../res/sons/Sound_Effects/hoverSound.mp3');
+        this.load.audio('selectSound', '../res/sons/Sound_Effects/MenuSelect.mp3');
     }
 
     create(data) {
+        this.hover = this.sound.add('hoverSound', {
+            mute: false,
+            volume: 1,
+            rate: 1,
+            detune: 0,
+            seek: 0,
+            loop: false,
+            delay: 0,
+        })
+
         this.add.image(1252 / 2, 834 / 2, 'background')
 
         const play = this.add.text(550, 680, 'PLAY!', { fontSize: '40px', fill: '#FFFFFF', fontFamily: 'Joystix' })
@@ -21,7 +34,10 @@ class MenuScreen extends Phaser.Scene {
                 "players": "one"
             }))
 
-        play.on('pointerover', () => play.setStyle({ fill: '#66bb6a' }));
+        play.on('pointerover', () => {
+            this.hover.play()
+            play.setStyle({ fill: '#66bb6a' })
+        });
         play.on('pointerout', () => play.setStyle({ fill: '#FFFFFF' }));
 
 
@@ -31,7 +47,10 @@ class MenuScreen extends Phaser.Scene {
                 "players": "two"
             }));
 
-        localMultiplayer.on('pointerover', () => localMultiplayer.setStyle({ fill: '#66bb6a' }));
+        localMultiplayer.on('pointerover', () => {
+            this.hover.play()
+            localMultiplayer.setStyle({ fill: '#66bb6a' })
+        });
         localMultiplayer.on('pointerout', () => localMultiplayer.setStyle({ fill: '#FFFFFF' }));
     }
 
